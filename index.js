6e1dd76487918e7f8440080905045bccd682e00e
atle <alextle24@gmail.com>
2024-01-12 17:20:23 -0700

    created recursive function to add all the sums found in array

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+        sum += item;
+    }
+  }
+  return sum;
+};
+
+console.log("2nd case",sumItems([[1, 2, [[3], 4]], 5, []]));
+console.log("3rd case",sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
+
+
 
 module.exports = sumItems;
